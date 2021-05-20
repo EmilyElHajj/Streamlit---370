36c45b3b55e918d1c003928709837217021c9c3a
EmilyElHajj <74826150+EmilyElHajj@users.noreply.github.com>
2021-05-20 09:32:04 +0300

    Update marketing.py

--- marketing.py
+++ marketing.py
@@ -75,8 +75,7 @@ df['Income_categories'] = np.select(conditions, values)
 df['Purchases']=df['MntWines']+df['MntFruits']+df['MntMeatProducts']+df['MntFishProducts']+df['MntSweetProducts']+df['MntGoldProds']
 df['Transactions']=df['NumWebPurchases']+df['NumCatalogPurchases']+df['NumStorePurchases']
 
-#df = df[df['Transactions'] > 1] #We keep customers with repeated purchases, implying number of transactions must be at least 2
-#df = df[df['Purchases'] > 0]
+
 
 df['AcceptedCmpTotal']=df['AcceptedCmp4']+df['AcceptedCmp5']+df['AcceptedCmp3']+df['AcceptedCmp2']+df['AcceptedCmp1']
 
